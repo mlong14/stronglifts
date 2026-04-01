39dce677f381a03d32077c424ab3fdc3d81dec82
Matt Long <matthew.w.long.14@gmail.com>
2026-03-31 22:16:00 -0700

    Initial Commit

---
+++ Stronglifts/StrongliftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrongliftsApp.swift
+//  Stronglifts
+//
+//  Created by Matthew Long on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct StrongliftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
